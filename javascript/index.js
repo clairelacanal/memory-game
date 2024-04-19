cbd492ec52fee9981469d38d8a363dd48a6e03c4
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-19 23:35:46 +0200

    text generation on going

--- javascript/index.js
+++ javascript/index.js
@@ -13,12 +13,18 @@ buttonEnter.addEventListener('click', function(){
 function getName(){
 const inputName = document.querySelector('#name').value;
 alert(inputName);
+}
 
 
 function getAge(){
 const inputAge = document.querySelector('#age').value;
 alert(inputAge);
 }
+
+//GENERE DU TEXTE DYNAMIQUEMENT
+const paragraphe = document.createElement('p');
+paragraphe.textContent = `Hello`;
+document.body.appendChild(paragraphe);
 
 
 
