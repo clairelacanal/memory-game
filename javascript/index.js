76b8dc2eb100a99993e392ee4c1c6cec5bb31058
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-19 22:55:44 +0200

    ajout des premieres fonctions

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,32 @@
-function buttonEnter(){
-    const buttonEnter = document.querySelector("#button-enter");
-    buttonEnter.addEventListener('click', function(){
-        console.log('bouton entré cliqué !');
-    })
+//HTML ELEMENTS
+const buttonEnter = document.querySelector('#button-enter');
+
+  
+//EVENEMENTS SUR LES BOUTONS
+buttonEnter.addEventListener('click', function(){
+    console.log('bouton entré cliqué !');
+    getName();
+    getAge();
+})
+
+//FONCTIONS
+function getName(){
+const inputName = document.querySelector('#name').value;
+alert(inputName);
+
+
+function getAge(){
+const inputAge = document.querySelector('#age').value;
+alert(inputAge);
 }
 
-function buttonStart(){}
+
+
+/*function buttonStart(){}
 
 function playWithCards(){}
 
 function exitGame(){}
 
-function buttonRestart(){}
+function buttonRestart(){}*/
 
