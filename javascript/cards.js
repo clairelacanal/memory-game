2d3e9f4be8dcfd768c1faecdb357c44b065c8e23
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-20 23:25:00 +0200

    bug pour faire apparaitre les cards

---
+++ javascript/cards.js
@@ -0,0 +1,10 @@
+const cards = [
+    {
+        name : "card1",
+        picture : "../images/cards/bougainvillea-8467373_640.jpg", 
+    },
+    {
+        name : "card2",
+        picture : "../images/cards/flowers-174817_640.jpg",
+    }
+];
--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,50 @@
 //HTML ELEMENTS
 const buttonEnter = document.querySelector('#button-enter');
+const buttonStart = document.querySelector('#button-start');
+
+
+
 
   
 //EVENEMENTS SUR LES BOUTONS
 buttonEnter.addEventListener('click', function(){
-    console.log('bouton entré cliqué !');
-    getName();
-    getAge();
+    displayText();
+})
+
+buttonStart.addEventListener('click',function(){
+    console.log('bouton start cliqué');
+    displayCards();
 })
 
 //FONCTIONS
-function getName(){
-const inputName = document.querySelector('#name').value;
+/*function getName(){
 alert(inputName);
-}
+}*/
 
 
-function getAge(){
+/*function getAge(){
 const inputAge = document.querySelector('#age').value;
 alert(inputAge);
+}*/
+
+function displayText(){
+const inputName = document.querySelector('#name').value;
+const paragraphe = document.querySelector('#display-text');
+paragraphe.textContent = `Hello ${inputName}, find the 36 pairs of flowers! Watch the clock! To start playing, press the start button."`;
 }
 
-//GENERE DU TEXTE DYNAMIQUEMENT
-const paragraphe = document.createElement('p');
-paragraphe.textContent = `Hello`;
-document.body.appendChild(paragraphe);
+function displayCards(){
+const randomIndex = Math.floor(Math.random() * cards.length);
+const splicedArr = cards.splice(randomIndex, 1);
+const randomCard = splicedArr[0];
+const cardsVisible = document.createElement('p');
+cardsVisible.innerHTML = `${randomCard.picture}`;
+p.appendChild(exampleRow);
+}
 
+/*function displayChrono(){
+   
+}*/
 
 
 /*function buttonStart(){}
