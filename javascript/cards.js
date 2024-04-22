388570b60b0c8b2371bdc34687d58838f7bf3361
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-22 11:37:02 +0200

    ajout des 20 img premier tableau

--- javascript/cards.js
+++ javascript/cards.js
@@ -1,10 +1,83 @@
 const cards = [
     {
         name : "card1",
-        picture : "../images/cards/bougainvillea-8467373_640.jpg", 
+        picture : "../images/cards/flower-card1.jpg", 
     },
     {
         name : "card2",
-        picture : "../images/cards/flowers-174817_640.jpg",
+        picture : "../images/cards/flower-card2.jpg",
+    },
+    {
+        name:"card3",
+        picture:"../images/cards/flower-card3.jpg",
+    },
+    {
+        name: "card4",
+        picture: "../images/cards/flower-card4.jpg",
+    },
+    {
+        name: "card5",
+        picture: "../images/cards/flower-card5.jpg",
+    },
+    {
+        name: "card6",
+        picture: "../images/cards/flower-card6.jpg",
+    },
+    {
+        name: "card7",
+        picture: "../images/cards/flower-card7.jpg",
+    },
+    {
+        name: "card8",
+        picture: "../images/cards/flower-card8.jpg",
+    },
+    {
+        name: "card9",
+        picture: "../images/cards/flower-card9.jpg",
+    },
+    {
+        name: "card10",
+        picture: "../images/cards/flower-card10.jpg",
+    },
+    {
+        name: "card11",
+        picture: "../images/cards/flower-card11.jpg",
+    },
+    {
+        name: "card12",
+        picture: "../images/cards/flower-card12.jpg",
+    },
+    {
+        name: "card13",
+        picture: "../images/cards/flower-card13.jpg",
+    },
+    {
+        name: "card14",
+        picture: "../images/cards/flower-card14.jpg",
+    },
+    {
+        name: "card15",
+        picture: "../images/cards/flower-card15.jpg",
+    },
+    {
+        name: "card16",
+        picture: "../images/cards/flower-card16.jpg",
+    },
+    {
+        name: "card17",
+        picture: "../images/cards/flower-card17.jpg",
+    },
+    {
+        name: "card18",
+        picture: "../images/cards/flower-card18.jpg",
+    },
+    {
+        name: "card19",
+        picture: "../images/cards/flower-card19.jpg",
+    },
+    {
+        name: "card20",
+        picture: "../images/cards/flower-card20.jpg",
     }
+    
 ];
