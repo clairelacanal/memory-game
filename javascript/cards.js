09dc1bf03a3033085802893eb79b6d0b24a144a6
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-25 15:50:03 +0200

    projet v5 done

--- javascript/cards.js
+++ javascript/cards.js
@@ -1,51 +1,51 @@
 const cards = [
     {
         name : "card1",
-        picture : "../images/cards/flower-card1.jpg", 
+        picture : "./images/cards/flower-card1.jpg", 
     },
     {
         name : "card2",
-        picture : "../images/cards/flower-card2.jpg",
+        picture : "./images/cards/flower-card2.jpg",
     },
     {
         name:"card3",
-        picture:"../images/cards/flower-card3.jpg",
+        picture:"./images/cards/flower-card3.jpg",
     },
     {
         name: "card4",
-        picture: "../images/cards/flower-card4.jpg",
+        picture: "./images/cards/flower-card4.jpg",
     },
     {
         name: "card5",
-        picture: "../images/cards/flower-card5.jpg",
+        picture: "./images/cards/flower-card5.jpg",
     },
     {
         name: "card6",
-        picture: "../images/cards/flower-card6.jpg",
+        picture: "./images/cards/flower-card6.jpg",
     },
     {
         name: "card7",
-        picture: "../images/cards/flower-card7.jpg",
+        picture: "./images/cards/flower-card7.jpg",
     },
     {
         name: "card8",
-        picture: "../images/cards/flower-card8.jpg",
+        picture: "./images/cards/flower-card8.jpg",
     },
     {
         name: "card9",
-        picture: "../images/cards/flower-card9.jpg",
+        picture: "./images/cards/flower-card9.jpg",
     },
     {
         name: "card10",
-        picture: "../images/cards/flower-card10.jpg",
+        picture: "./images/cards/flower-card10.jpg",
     },
     {
         name: "card11",
-        picture: "../images/cards/flower-card11.jpg",
+        picture: "./images/cards/flower-card11.jpg",
     },
     {
         name: "card12",
-        picture: "../images/cards/flower-card12.jpg",
+        picture: "./images/cards/flower-card12.jpg",
     },
     
     
